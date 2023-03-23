fda73ff3c44c8de87b82f742e1b903481cc59b41
AlexQwerty73 <124087147+AlexQwerty73@users.noreply.github.com>
2023-03-23 21:17:32 +0100

    contacts

--- script/main.js
+++ script/main.js
@@ -39,3 +39,12 @@ $('.slider').slick({
     slidesToShow: count,
     slidesToScroll: count
 });
+
+
+function myMap() {
+    var mapCanvas = document.getElementById("map");
+    var mapOptions = {
+      center: new google.maps.LatLng(51.5, -0.2), zoom: 10
+    };
+    var map = new google.maps.Map(mapCanvas, mapOptions);
+  }
